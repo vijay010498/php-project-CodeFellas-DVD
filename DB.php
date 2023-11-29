ad09f4160514711d23bbef3006b4728d71f48a59
vijay010498 <svijayr007@gmail.com>
2023-11-29 13:22:08 -0500

    add:added imageURL in DVDs table

--- DB.php
+++ DB.php
@@ -77,6 +77,7 @@ class DB
                 GenreId INT NOT NULL,
                 Price DECIMAL(10, 2) NOT NULL,
                 StockQuantity INT DEFAULT 0,
+                imageURL varchar(255),
                 FOREIGN KEY (GenreId) REFERENCES Genres(GenreId)        
             )
         ");
